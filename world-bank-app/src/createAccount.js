57e9cdaa3bdce1bea70ec4942dcb56eda44dca37
Mohammad Hazrati <mohhaz64@gmail.com>
2021-10-21 16:45:58 +0100

    Fixed

--- world-bank-app/src/createAccount.js
+++ world-bank-app/src/createAccount.js
@@ -1,10 +1,6 @@
 import { useFormik } from "formik"
 import * as Yup from "yup"
-<<<<<<< HEAD
 import { useState } from "react"
-=======
->>>>>>> 7d9ce23d6b84ee87c85df24fd6329b28be26ffd6
-import "bootstrap/dist/css/bootstrap.min.css"
 
 import { Button, Form, Row, Col, Alert } from "react-bootstrap"
 
@@ -17,32 +13,22 @@ function CreateAccount() {
             confirmPassword: "",
         },
         validationSchema: Yup.object({
-            password: Yup.string()
-                .min(8, "Must be at least 8 characters")
-                .max(20, "Must be 20 characters or less")
-                .required("Required"),
+            password: Yup.string().min(8, "Must be at least 8 characters").max(20, "Must be 20 characters or less").required("Required"),
             confirmPassword: Yup.string()
                 .oneOf([Yup.ref("password"), null], "Passwords must match")
                 .required("Required"),
-            email: Yup.string()
-                .max(35, "Cannot exceed 35 characters")
-                .email("Invalid email address")
-                .required("Required"),
+            email: Yup.string().max(35, "Cannot exceed 35 characters").email("Invalid email address").required("Required"),
         }),
         onSubmit: async (values) => {
             const result = await sendUserDetailsToServer(values)
-            result === "Success"
-                ? setServerResponseMessage("Success")
-                : setServerResponseMessage("Failure")
+            result === "Success" ? setServerResponseMessage("Success") : setServerResponseMessage("Failure")
             //Redirect to search page
         },
     })
 
     return (
         <div className="d-flex justify-content-center">
-            <Form
-                data-testid="createAccountForm"
-                onSubmit={formik.handleSubmit}>
+            <Form data-testid="createAccountForm" onSubmit={formik.handleSubmit}>
                 <Row className="mb-3">
                     <Form.Group>
                         <Form.Control
@@ -51,22 +37,13 @@ function CreateAccount() {
                             type="email"
                             data-testid="createUsername"
                             placeholder="email"
-                            className={validationCSS(
-                                formik.touched.email,
-                                formik.errors.email
-                            )}
+                            className={validationCSS(formik.touched.email, formik.errors.email)}
                             onChange={formik.handleChange}
                             onBlur={formik.handleBlur}
                             value={formik.values.email}
                         />
                         {formik.touched.email && formik.errors.email ? (
-<<<<<<< HEAD
-                            <div
-                                className="invalid-feedback"
-                                data-testid="emailError">
-=======
-                            <div data-testid="emailError">
->>>>>>> 7d9ce23d6b84ee87c85df24fd6329b28be26ffd6
+                            <div className="invalid-feedback" data-testid="emailError">
                                 {formik.errors.email}
                             </div>
                         ) : null}
@@ -81,23 +58,13 @@ function CreateAccount() {
                                 type="password"
                                 data-testid="createPassword"
                                 placeholder="password"
-                                class={validationCSS(
-                                    formik.touched.password,
-                                    formik.errors.password
-                                )}
+                                class={validationCSS(formik.touched.password, formik.errors.password)}
                                 onChange={formik.handleChange}
                                 onBlur={formik.handleBlur}
                                 value={formik.values.password}
                             />
-                            {formik.touched.password &&
-                            formik.errors.password ? (
-<<<<<<< HEAD
-                                <div
-                                    className="invalid-feedback"
-                                    data-testid="requirePassword">
-=======
-                                <div data-testid="requirePassword">
->>>>>>> 7d9ce23d6b84ee87c85df24fd6329b28be26ffd6
+                            {formik.touched.password && formik.errors.password ? (
+                                <div className="invalid-feedback" data-testid="requirePassword">
                                     {formik.errors.password}
                                 </div>
                             ) : null}
@@ -111,37 +78,21 @@ function CreateAccount() {
                                 type="password"
                                 data-testid="confirmPassword"
                                 placeholder="confirm password"
-                                class={validationCSS(
-                                    formik.touched.confirmPassword,
-                                    formik.errors.confirmPassword
-                                )}
+                                class={validationCSS(formik.touched.confirmPassword, formik.errors.confirmPassword)}
                                 onChange={formik.handleChange}
                                 onBlur={formik.handleBlur}
                                 value={formik.values.confirmPassword}
                             />
-                            {formik.touched.confirmPassword &&
-                            formik.errors.confirmPassword ? (
-<<<<<<< HEAD
-                                <div
-                                    className="invalid-feedback"
-                                    data-testid="requireConfirmPassword">
-=======
-                                <div data-testid="requireConfirmPassword">
->>>>>>> 7d9ce23d6b84ee87c85df24fd6329b28be26ffd6
+                            {formik.touched.confirmPassword && formik.errors.confirmPassword ? (
+                                <div className="invalid-feedback" data-testid="requireConfirmPassword">
                                     {formik.errors.confirmPassword}
                                 </div>
                             ) : null}
                         </Form.Group>
                     </Col>
                 </Row>
-<<<<<<< HEAD
                 <div className="d-grid">
-                    <Button
-                        className="mb-3"
-                        variant="primary"
-                        type="submit"
-                        data-testid="submitCreateAccount"
-                        size="lg">
+                    <Button className="mb-3" variant="primary" type="submit" data-testid="submitCreateAccount" size="lg">
                         Create Account
                     </Button>
                     {serverResponseMessage === "Success" ? (
@@ -151,9 +102,8 @@ function CreateAccount() {
                     ) : serverResponseMessage === "Failure" ? (
                         <Alert variant="danger" data-testid="redirectMessage">
                             <p>
-                                Sorry, there may already be an account under
-                                that name. Alternatively, we're having trouble
-                                accessing our server at the moment.
+                                Sorry, there may already be an account under that name. Alternatively, we're having trouble accessing our server at
+                                the moment.
                             </p>
                             <hr />
                             <p>Please wait a few moments and try again.</p>
@@ -161,35 +111,6 @@ function CreateAccount() {
                     ) : null}
                 </div>
             </Form>
-=======
-                <Button
-                    variant="primary"
-                    type="submit"
-                    data-testid="submitCreateAccount"
-                    size="lg">
-                    Create Account
-                </Button>
-            </Form>
-<<<<<<< HEAD
-            {serverResponseMessage === "Success" ? (
-                <div data-testid="redirectMessage">
-                    Success. Redirecting....
-                </div>
-            ) : serverResponseMessage === "Failure" ? (
-                <div data-testid="redirectMessage">
-                    <p>
-                        Sorry, there may already be an account under that name.
-                        Alternatively, we're having trouble accessing our server
-                        at the moment.
-                    </p>
-                    <p>Please wait a few moments and try again.</p>
-                </div>
-            ) : (
-                ""
-            )}
-=======
->>>>>>> 7d9ce23d6b84ee87c85df24fd6329b28be26ffd6
->>>>>>> main
         </div>
     )
 }
