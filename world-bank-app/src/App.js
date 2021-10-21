dfafefb32859d748f30ce11358ec797679be51af
Mohammad Hazrati <mohhaz64@gmail.com>
2021-10-21 09:17:46 +0100

    Changed how boostrap is imported and added react router

--- world-bank-app/src/App.js
+++ world-bank-app/src/App.js
@@ -3,32 +3,24 @@ import CreateAccount from "./CreateAccount"
 import Login from "./Login"
 import MainSearch from "./MainSearch"
 
-import { default as Router, Route, Switch } from "react-router"
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom"
 
 function App() {
     return (
-        <div className="text-center">
-            <head>
-                <link
-                    rel="stylesheet"
-                    href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css"
-                    integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS"
-                    crossorigin="anonymous"></link>
-            </head>
-            <img
-                src="https://www.bellanaija.com/wp-content/uploads/2015/08/World-Bank-logo.jpg"
-                class="w-25"
-                alt="worldBankLogo"
-            />
-
-            <Router>
+        <Router>
+            <div className="text-center">
+                <img
+                    src="https://www.bellanaija.com/wp-content/uploads/2015/08/World-Bank-logo.jpg"
+                    class="w-25"
+                    alt="worldBankLogo"
+                />
                 <Switch>
                     <Route exact path="/" component={Login} />
                     <Route path="/createAccount" component={CreateAccount} />
                     <Route path="/mainSearch" component={MainSearch} />
                 </Switch>
-            </Router>
-        </div>
+            </div>
+        </Router>
     )
 }
 
---
+++ world-bank-app/src/Components/SearchNavButton.js
@@ -0,0 +1,6 @@
+import React from "react"
+import Button from "react-bootstrap"
+
+export default function SearchNavButton() {
+    return <Button variant="secondary">Search</Button>
+}
